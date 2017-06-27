acc8d14d0818fb5eff1da6427082bde9b167bb69
wbdana <william.b.dana@gmail.com>
2017-06-27 17:44:31 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    erb :team
+  end
+  #
+  # get '/team' do
+  #   erb :team
+  # end
+
+  #
+  # post '/newteam' do
+  #   erb :team
+  # end
 
 end
